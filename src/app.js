57854773f4f4f3fc1ed4edffd77a86be5f1a0ca4
Pablo <cont.pab@gmail.com>
2022-01-21 23:53:04 -0300

    Generador de nombres completo

--- src/app.js
+++ src/app.js
@@ -7,5 +7,29 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+
+  let newArr = [];
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let i = 0; i < adj.length; i++) {
+      for (let i = 0; i < noun.length; i++) {
+        newArr.push(`${pronoun[i]}${adj[i]}${noun[i]}.com`);
+      }
+    }
+    //newArr.push(pronoun[i]);
+  }
+  console.log(newArr);
+
+  for (let i = 0; i < newArr.length; i++) {
+    let total = newArr[i];
+    console.log(total);
+
+    let excusa = document.createElement("p");
+    excusa.innerHTML = `${total}`;
+    let container = document.getElementById("container");
+    //console.log(container);
+    container.append(excusa);
+  }
 };
